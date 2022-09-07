d419f9cf311b236e5d77263b66d7e5e2e305b499
MTahaOzsahin <87945619+MTahaOzsahin@users.noreply.github.com>
2022-09-07 11:45:49 +0300

    Added new menu ui.
    Bakcup before try something explosive :)

--- 2048 Prototype/Assets/GameFolders/Scripts/Managers/ScriptableObjects/GameManager.cs
+++ 2048 Prototype/Assets/GameFolders/Scripts/Managers/ScriptableObjects/GameManager.cs
@@ -8,15 +8,17 @@ namespace Prototype.Scripts.Managers.ScriptableObjects
     [CreateAssetMenu(menuName ="Managers/GameManager")]
     public class GameManager : ScriptableObject
     {
+        [SerializeField] SoundManager soundManager;
+        [SerializeField] ScoreManager scoreManager;
         public void ExitApplication()
         {
             Application.Quit();
         }
         public void Restart()
         {
+            scoreManager.currentScore = 0;
             int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.UnloadSceneAsync(currentLevelIndex);
-            SceneManager.LoadSceneAsync(currentLevelIndex);
+            SceneManager.LoadSceneAsync(currentLevelIndex,LoadSceneMode.Single);
         }
     }
 }
